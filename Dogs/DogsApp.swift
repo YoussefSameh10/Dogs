e49ea9392545d8c37fecc7494d6fc94f6e8dbefe
Youssef Ghattas <youssef.ghattas@integrant.com>
2024-03-24 16:48:30 +0200

    Initial Commit

---
+++ Dogs/DogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogsApp.swift
+//  Dogs
+//
+//  Created by Youssef Ghattas on 24/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct DogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
